17868e388457f482b626c206776a6b625ae06251
CyberS <stservais0409@gmail.com>
2025-05-20 15:15:54 +0200

    Create auth middleware

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,50 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+// Protect routes
+module.exports = async (req, res, next) => {
+  let token;
+  
+  // Check if token exists in headers
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    // Get token from headers
+    token = req.headers.authorization.split(' ')[1];
+  }
+  
+  // Check if token exists
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: 'Not authorized to access this route'
+    });
+  }
+  
+  try {
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    // Set user in request
+    req.user = decoded.user;
+    
+    // Check if user exists
+    const user = await User.findById(req.user.id);
+    
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: 'User not found'
+      });
+    }
+    
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      success: false,
+      message: 'Not authorized to access this route'
+    });
+  }
+};
